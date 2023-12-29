fceae3ab27d89290fb1f484d320e2405f0881687
anatoliy2221 <anatoliy2221@ukr.net>
2023-12-29 20:13:02 +0200

    Update task-4

--- js/task-4.js
+++ js/task-4.js
@@ -4,16 +4,16 @@ loginForm.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
     event.preventDefault();
     const form = event.target;
-    const email = form.elements.email.value.trim();
-    const password = form.elements.password.value.trim();
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
     if (email === "" || password === "") {
         alert("All form fields must be filled in");
     } else {
-        const obj = {
-            email,
-            password,
+        const resultValue = {
+            email: email.trim(),
+            password: password.trim(),
         };
-        console.log(obj);
+        console.log(resultValue);
         form.reset();
     }
 }
